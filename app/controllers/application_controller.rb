1dc23296cf4f4bed6ba4dbde1e41ddd74328662e
setoneno <setone0222@gmail.com>
2018-09-27 10:12:47 +0000

    follow, unfollw

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,7 @@ class ApplicationController < ActionController::Base
 
   def counts(user)
     @count_photos = user.photos.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
 end
